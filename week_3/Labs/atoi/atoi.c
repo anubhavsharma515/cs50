e0662db992ce8a2f7593e0f5d82b3a40b1757075
Anubhav Sharma <anubhav.sharma@wattpad.com>
2023-07-04 00:24:48 -0400

    Add week 3 atoi Lab

---
+++ week_3/Labs/atoi/atoi.c
@@ -0,0 +1,40 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int convert(string input);
+
+int main(void)
+{
+    string input = get_string("Enter a positive integer: ");
+
+    for (int i = 0, n = strlen(input); i < n; i++)
+    {
+        if (!isdigit(input[i]))
+        {
+            printf("Invalid Input!\n");
+            return 1;
+        }
+    }
+
+    // Convert string to int
+    printf("%i\n", convert(input));
+}
+
+int convert(string input)
+{
+    int length = strlen(input);
+
+    // TODO
+
+    if (length == 0)
+    {
+        return 0;
+    }
+
+    int digit = input[length -1] - 48;
+    input[length - 1] = input[length];
+    return digit + (10 * convert(input));
+}
