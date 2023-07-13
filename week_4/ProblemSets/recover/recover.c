15c3ab7bbcc70b284296569a5572da791ed98799
Anubhav Sharma <anubhav.sharma@wattpad.com>
2023-07-13 01:01:01 -0400

    Fix valgrind error

--- week_4/ProblemSets/recover/recover.c
+++ week_4/ProblemSets/recover/recover.c
@@ -37,6 +37,10 @@ int main(int argc, char *argv[])
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
+            if (outptr != NULL)
+            {
+                fclose(outptr);
+            }
             sprintf(filename, "%03i.jpg", counter);
             outptr = fopen(filename, "w"); 
             ++counter;
